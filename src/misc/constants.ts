221f639bd1a97b4a462eb4f67d3451b59bfaea25
Danila Simonov <ek@ek-mail.ru>
2023-07-17 13:22:12 +0200

    ankr for gnosis

--- src/misc/constants.ts
+++ src/misc/constants.ts
@@ -159,6 +159,10 @@ export const EVM_RPCS: Record<
 	[EVMNetwork.GNOSIS]: [
 		{ rpc: 'https://rpc.gnosischain.com', blockLimit: 10000 },
 		{ rpc: 'wss://rpc.gnosischain.com/wss', blockLimit: 10000 },
+		{
+			rpc: 'https://rpc.ankr.com/gnosis',
+			blockLimit: 1000,
+		},
 		{ rpc: 'https://cosmopolitan-dawn-sky.xdai.discover.quiknode.pro/4405e640361b4c7ef17a3f07431647fd6375fc29/' },
 		{ rpc: 'https://xdai-rpc.gateway.pokt.network' },
 		{ rpc: 'https://gnosis-mainnet.public.blastapi.io' },
